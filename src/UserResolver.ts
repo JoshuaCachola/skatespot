217813922227d5a79868b5610847f19d6fe91b17
Joshua Cachola <joshuaelliscachola@gmail.com>
2021-03-23 18:40:10 -0700

    add login mutation

--- src/UserResolver.ts
+++ src/UserResolver.ts
@@ -1,30 +1,67 @@
-import {Arg, Mutation, Query, Resolver} from 'type-graphql';
-import argon2 from 'argon2';
-import { User } from './entity/User';
+import {
+  Arg,
+  Field,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from "type-graphql";
+import argon2 from "argon2";
+import { User } from "./entity/User";
+import { sign } from "jsonwebtoken";
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string;
+}
 
 @Resolver()
 export class UserResolver {
   @Query(() => String)
   hello() {
-    return 'hi'
+    return "hi";
   }
 
   @Mutation(() => Boolean)
   async register(
-    @Arg('email') email: string,
-    @Arg('password') password: string
+    @Arg("email") email: string,
+    @Arg("password") password: string
   ) {
     try {
       const hashedPassword = await argon2.hash(password);
       await User.insert({
         email,
-        password: hashedPassword
+        password: hashedPassword,
       });
     } catch (err) {
       console.error(err);
       return false;
     }
     return true;
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(@Arg("email") email: string, @Arg("password") password: string) {
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return {
+        accessToken: "",
+      };
+    }
+
+    if (await argon2.verify(user.password, password)) {
+      const accessToken = sign({ userId: user.id }, "change-in-prod", {
+        expiresIn: "15m",
+      });
+      return {
+        accessToken,
+      };
+    } else {
+      return {
+        accessToken: "",
+      };
+    }
   }
 
   @Query(() => [User])
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
-import 'reflect-metadata';
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import { buildSchema } from 'type-graphql';
-import { UserResolver } from './UserResolver';
-import { createConnection, getConnectionOptions } from 'typeorm';
+import "reflect-metadata";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./UserResolver";
+import { createConnection, getConnectionOptions } from "typeorm";
 
 (async () => {
-	const app = express();
-	app.get('/ping', (_, res) => res.send('pong'));
+  const app = express();
+  app.get("/ping", (_, res) => res.send("pong"));
 
-	await createConnection(await getConnectionOptions('default'));
+  const connection = await getConnectionOptions("default");
+  await createConnection(connection);
 
-	const apolloServer = new ApolloServer({
-		schema: await buildSchema({
-			resolvers: [UserResolver]
-		})
-	});
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver],
+    }),
+  });
 
-	apolloServer.applyMiddleware({ app });
-	app.listen(4000, () => {
-		console.log('express server started...')
-	});
+  apolloServer.applyMiddleware({ app });
+  app.listen(4000, () => {
+    console.log("express server started...");
+  });
 })();
-
