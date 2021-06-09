e33e36cca1b2eb4a798e861e9d02298c79f76c11
Joshua Cachola <joshuaelliscachola@gmail.com>
2021-06-09 14:37:04 -0700

    update: port on server start script

--- server/src/utils/typeormConnection.ts
+++ server/src/utils/typeormConnection.ts
@@ -1,9 +1,18 @@
 import { Connection, createConnection, getConnection, getConnectionOptions } from 'typeorm';
+// const aws = require('aws-sdk');
+
+// const signer = new aws.RDS.({
+
+// });
 
 export const typeormConnection = {
   async create(): Promise<Connection> {
     const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
 
+    console.log(process.env.NODE_ENV);
+    console.log(process.env.DATABASE_HOST);
+    console.log(process.env.DATABASE_PASSWORD);
+    console.log(process.env.DATABASE_USERNAME);
     if (process.env.NODE_ENV === 'production') {
       connectionOptions['host'] = process.env.DATABASE_HOST;
       connectionOptions['password'] = process.env.DATABASE_PASSWORD;
